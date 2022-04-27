71ca0f414d23072193ec033481de6b4256597bc5
Martino888 <maxmartino33@gmail.com>
2022-04-27 23:11:29 +0200

    non stampa icone e jambo

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,32 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    $card = config('comics');
+    $data = [
+        'card' => $card,
+        'info' => [
+            [
+                'img' => 'img/buy-comics-digital-comics.png',
+                'text' => 'DIGITAL COMICS',
+            ],
+            [
+                'img' => 'img/buy-comics-merchandise.png',
+                'text' => 'DC MERCHANDISE',
+            ],
+            [
+                'img' => 'img/buy-comics-subscriptions.png',
+                'text' => 'SUBSCRIPTION',
+            ],
+            [
+                'img' => 'img/buy-comics-shop-locator.png',
+                'text' => 'COMIC SHOP LOCATOR',
+            ],
+            [
+                'img' => 'img/buy-dc-power-visa.svg',
+                'text' => 'DC POWER VISA',
+            ],
+        ],
+    ];
+    return view('home', $data);
+})-> name('home');
